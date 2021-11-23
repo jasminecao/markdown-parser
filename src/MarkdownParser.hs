f65dc068e4278c39db64a08118ca4ae7bddf377e
Jasmine Cao <jcao3@seas.upenn.edu>
2021-11-23 18:50:42 -0500

    refactoring types

--- src/MarkdownParser.hs
+++ src/MarkdownParser.hs
@@ -1,59 +1,70 @@
 module MarkdownParser where
 
 import Data.Char (isSpace)
-import Syntax (Content (E, Text), EType (..), reservedMarkdownChars)
+import Syntax (Block (..), Doc (Doc), Line, Text (..), reservedMarkdownChars)
+import qualified Syntax as S
 import Text.Parsec.Token
 import Text.ParserCombinators.Parsec
 
 -- TODO: add error filepath arg to parse
-parseMarkdown :: String -> Either ParseError Content
+parseMarkdown :: String -> Either ParseError Doc
 parseMarkdown = parse markdownP ""
 
-markdownP :: Parser Content
-markdownP = blockP
+markdownP :: Parser Doc
+markdownP = Doc <$> many blockP
 
+-- TODO: add end of line check (\n)
 -- parses for a block of markdown (headings, lists, quotes, code blocks)
-blockP :: Parser Content
-blockP = headingP <|> ulListP <|> olListP <|> quoteP <|> codeBlockP <|> lineP
+blockP :: Parser Block
+blockP = headingP <|> ulListP <|> olListP <|> quoteP <|> codeBlockP <|> paragraphP
 
 -- parses for # heading and converts rest of line to Text
-headingP :: Parser Content
+headingP :: Parser Block
 headingP = do
   hx <- wsP $ many1 (char '#')
-  E (Heading (length hx)) <$> lineP
+  Heading (length hx) <$> lineP
 
-ulListP :: Parser Content
+ulListP :: Parser Block
 ulListP = undefined
 
-olListP :: Parser Content
+olListP :: Parser Block
 olListP = undefined
 
-quoteP :: Parser Content
+quoteP :: Parser Block
 quoteP = undefined
 
 -- quoteP = Element BlockQuote <$> many (between (string ">") (string ">") textP)
 
-codeBlockP :: Parser Content
-codeBlockP = E CodeBlock <$> between (string "```") (string "```") textP
+paragraphP :: Parser Block
+paragraphP = undefined
+
+codeBlockP :: Parser Block
+codeBlockP = CodeBlock <$> between (string "```") (string "```") stringP
 
 -- parses a line to handle style (bold, italics, etc), inline code
-lineP :: Parser Content
-lineP = boldP <|> italicP <|> strikeP <|> inlineCodeP <|> textP
+lineP :: Parser S.Line
+lineP = S.Line <$> many textP
 
-boldP :: Parser Content
-boldP = E Bold <$> between (string "**") (string "**") textP
+textP :: Parser Text
+textP = boldP <|> italicP <|> strikeP <|> inlineCodeP <|> normalP
 
-italicP :: Parser Content
-italicP = E Italic <$> between (char '*') (char '*') textP
+boldP :: Parser Text
+boldP = Bold <$> between (string "**") (string "**") stringP
 
-strikeP :: Parser Content
-strikeP = E Strikethrough <$> between (char '~') (char '~') textP
+italicP :: Parser Text
+italicP = Italic <$> between (char '*') (char '*') stringP
 
-inlineCodeP :: Parser Content
-inlineCodeP = E InlineCode <$> between (char '`') (char '`') textP
+strikeP :: Parser Text
+strikeP = Strikethrough <$> between (char '~') (char '~') stringP
 
-textP :: Parser Content
-textP = Text <$> many1 (satisfy notReservedChar)
+inlineCodeP :: Parser Text
+inlineCodeP = InlineCode <$> between (char '`') (char '`') stringP
+
+normalP :: Parser Text
+normalP = Normal <$> stringP
+
+stringP :: Parser String
+stringP = many1 (satisfy notReservedChar)
   where
     notReservedChar c = c `notElem` reservedMarkdownChars
 
--- src/Syntax.hs
+++ src/Syntax.hs
@@ -2,18 +2,15 @@
 
 module Syntax where
 
-newtype Doc = Doc [E]
+newtype Doc = Doc [Block]
   deriving (Eq, Show)
 
-data E = 
-    Heading Int Block
-  | Paragraph Block -- p
-  | OrderedList [Block]
-  | UnorderedList [Block]
-  | Link String Block -- a
-  | Container [E] -- needed??? div, section, main
-  | Head E
-  | Body E
+data Block
+  = Heading Int Line
+  | Paragraph Line -- p
+  | OrderedList [Line]
+  | UnorderedList [Line]
+  | Link String Line -- a
   | BlockQuote String
   | CodeBlock String
   | Hr -- empty
@@ -21,24 +18,24 @@ data E =
   | Table TableType
   deriving (Eq, Show)
 
--- this will be a monoid
-newtype Block = Block [Text]
+data TableType
+  = TableHead [TableType]
+  | TableBody [TableType]
+  | TableRow [TableType]
+  | TableCell Line
   deriving (Eq, Show)
 
-data Text =
-    Bold String
+-- this will be a monoid
+newtype Line = Line [Text]
+  deriving (Eq, Show)
+
+data Text
+  = Bold String
   | Italic String
   | Underline String
   | Strikethrough String
   | InlineCode String
   | Normal String
-  deriving (Eq, Show)
-
-data TableType
-  = TableHead [TableType]
-  | TableBody [TableType]
-  | TableRow [TableType]
-  | TableCell Block
   deriving (Eq, Show)
 
 reservedMarkdownChars :: [Char] =
